c5f436b3bb89859ef7692db2caf3edf1d503f82f
Ancil Marshall <ancil.marshall@bioserenity.com>
2017-05-29 15:30:41 +0200

    update example 13.2 and 13.2b

---
+++ 2_kinematics/testQuaternion.m
@@ -0,0 +1,35 @@
+% original euler angles
+psi0 = pi/4;theta0 = 0.5*pi;phi0 = pi/3;
+
+% form direction cosine matrix 3-1-3
+C = dcm3(phi0)*dcm1(theta0)*dcm3(psi0);
+q = quaternion(C);
+
+% check with built in matlab functions
+Cstd = angle2dcm(psi0,theta0,phi0,'ZXZ');
+qstd = dcm2quat(Cstd);
+
+function C = dcm3(x)
+c = cos(x);
+s = sin(x);
+C = [c s 0;
+    -s c 0;
+     0 0 1];
+end
+
+function C = dcm1(x)
+c = cos(x);
+s = sin(x);
+C = [1  0 0;
+     0  c s;
+     0 -s c];
+end
+
+function qnew = toStd(q)
+    qnew = [q(4) q(1:3)];
+end
+
+function qnew = toNormal(q)
+    qnew = [q(2:4) q(1)];
+end
+
